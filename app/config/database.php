4a5f37971de40ee051ad275091d0fcd5c0f1c635
mattnkofi <justinebolanos1018@gmail.com>
2025-09-11 00:04:11 +0800

    third commit

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'test',
+    'username'	=> 'sql12798074',
+    'password'	=> '8C3ueY2u9c',
+    'database'	=> 'sql12798074',
     // 'database'	=> 'mockdata',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
