1d9734cbc28a4bc0ee966121e9810f29c2e0ce8c
mattnkofi <justinebolanos1018@gmail.com>
2025-09-10 23:57:00 +0800

    second commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1a7c4cdd6b2ab273e2d9174674c474da48d8fd15',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1a7c4cdd6b2ab273e2d9174674c474da48d8fd15',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
