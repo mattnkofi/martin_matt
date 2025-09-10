f05a819f1c8d827fe8487f8d1db64b029feeb34c
mattnkofi <justinebolanos1018@gmail.com>
2025-09-11 00:19:46 +0800

    third commit

--- scheme/libraries/Session/FileSessionHandler.php
+++ scheme/libraries/Session/FileSessionHandler.php
@@ -81,6 +81,8 @@ class FileSessionHandler extends Session implements SessionHandlerInterface {
         $this->save_path = $save_path;
         $this->file_path = $this->save_path.DIRECTORY_SEPARATOR.$session_name . '_';
         if ( !is_dir($this->save_path) ) {
+            var_dump($this->save_path);
+            exit;
             mkdir($this->save_path, 0700, TRUE);
         }
         return true;
